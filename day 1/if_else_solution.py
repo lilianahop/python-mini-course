d5de5a911011ec78c89289b3804f3e78c78b422c
lilianahop <hopkins.liliana@gmail.com>
2023-06-01 17:08:44 -0400

    Completed Day 2

--- day 1/if_else_solution.py
+++ day 1/if_else_solution.py
@@ -11,21 +11,40 @@
 #     print("It's false")
 
 
-num = 1
+num = 5
 print("Num is ", num)
 if num == 0:
     print("It's 0!")
-if num == 1:
+elif num == 1:
     print("It's 1!")
+else: 
+    print("I don't know.")
 
 # and
+if False and False:
+    print("It's False.")
 
 # or
+if True or False: 
+    print("It's definitely something.")
+else:
+    print("It's false.")
 
 # not
+if not False:
+    print("It's true!")
+if not True:
+    print("It's False!")
 
 # comparision == !=
 
 # operators += -= *= /= %= //= **=
 
 # switch (match) statements
+
+color = "blue"
+
+if color == "red" or color == "pink":
+    print("It's reddish")
+else:
+    print("It's not reddish")
--- day 2/rockScissorsPaper.py
+++ day 2/rockScissorsPaper.py
@@ -15,9 +15,38 @@ def computerChoice():
 
 def rockScissorsPaper():
   #Code Here
+  user_choice = input("choose Rock or Scissors or Paper: ")
+  print(user_choice)
+  if user_choice == "Rock" or user_choice == "Paper" or user_choice == "Scissors": 
+    print("Valid")
+  else:
+    print("Invalid")
+  
+  computer_response = whatIsIt(computerChoice())
+  print(computer_response)
+  if computer_response == user_choice:
+    print("It's a tie.")
+  else: 
+    if user_choice == "Rock":
+      if computer_response == "Scissors":
+        print("User won")
+      if computer_response == "Paper":
+        print("Computer won")
+    elif user_choice == "Scissors":
+      if computer_response == "Rock":
+        print("Computer won")
+      if computer_response == "Paper":
+        print("User won")
+    elif user_choice == "Paper":
+      if computer_response == "Rock":
+        print("Computer won")
+      if computer_response == "Scissors":
+        print("User won")
+  
+  
 
   #print random computer response
-  print(whatIsIt(computerChoice()))
+  # print(whatIsIt(computerChoice()))
 
 rockScissorsPaper()
 
